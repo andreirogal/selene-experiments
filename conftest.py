d26e8372e195743864b03e18380fadb9e096af44
Andrei Rogal <andreirogal11@gmail.com>
2020-02-15 19:08:32 +0300

    add time tracking for fixtures

--- conftest.py
+++ conftest.py
@@ -1,23 +1,45 @@
 # -*- coding: utf-8 -*-
 
+import logging
+import time
+
 import pytest
 from selene.support.shared import browser
 from selenium import webdriver
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@pytest.fixture(autouse=True, scope='session')
+def session_scope():
+    start = time.time()
+    yield
+    logger.info(f'session duration : {time.time() - start} seconds')
+
 
 @pytest.fixture(scope="function")
 def selene_config():
+    start = time.time()
+    logger.info(f'start config browser : {time.time() - start} seconds')
     browser.config.browser_name = "chrome"
     browser.config.base_url = "https://google.com"
     browser.config.timeout = 2
     browser.config.reports_folder = "selene_reports"
+    logger.info(f'browser config duration: {time.time() - start} seconds')
     yield
     browser.quit()
+    logger.info(f'test duration : {time.time() - start} seconds')
 
 
 @pytest.fixture(scope='function')
 def native_selenium():
+    start = time.time()
+    logger.info(f'start {start}')
+    logger.info(f'before start browser: {time.time() - start} seconds')
     browser = webdriver.Chrome()
+    logger.info(f'browser start duration: {time.time() - start} seconds')
     browser.implicitly_wait(5)
     yield browser
     browser.quit()
+    logger.info(f'test duration : {time.time() - start} seconds')
